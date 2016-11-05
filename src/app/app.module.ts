d11114048c4fc212b1ff3837bf13e51ed2a8c472
Tom Pain <tombpain@gmail.com>
2016-11-05 17:27:41 +0000

    adding components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/global/header.component';
+import { PrimaryNavComponent } from './components/navigation/primary-nav.component';
+import { SecondaryNavComponent } from './components/navigation/secondary-nav.component';
+import { SearchComponent } from './components/navigation/search.component';
+import { CallToActionComponent } from './components/messaging/call-to-action.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PrimaryNavComponent,
+    SecondaryNavComponent,
+    SearchComponent,
+    CallToActionComponent
   ],
   imports: [
     BrowserModule,
